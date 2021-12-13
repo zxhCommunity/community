eda3de0d3cf1d600faf5ae0da487fdf81c40b0ab
ze_hua <1696901745@qq.com>
2021-12-13 20:03:44 +0800

    更新

--- src/main/java/com/zxh/community/controller/AuthorizeController.java
+++ src/main/java/com/zxh/community/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 /**
@@ -35,7 +37,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                            HttpServletRequest request
+                           // HttpServletRequest request,
+                           HttpServletResponse response
     ){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
@@ -48,13 +51,15 @@ public class AuthorizeController {
         System.out.println(githubUser.getName());
         if(githubUser!=null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            request.getSession().setAttribute("user",githubUser);
+            response.addCookie(new Cookie("token",token));//登陆成功后把token写入Cookie
+           // request.getSession().setAttribute("user",githubUser);
             return "redirect:/";
         }else{
             return "redirect:/";
--- src/main/java/com/zxh/community/mapper/UserMapper.java
+++ src/main/java/com/zxh/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.zxh.community.mapper;
 import com.zxh.community.module.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * @author zxh
@@ -12,4 +14,7 @@ import org.apache.ibatis.annotations.Mapper;
 public interface UserMapper {
     @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("Select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
